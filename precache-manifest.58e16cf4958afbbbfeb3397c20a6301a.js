fc874876e839a852cdd6e99f9b07a364dd51f70a
Sammy Jackson <sam.jackson@live.ca>
2019-10-07 00:00:39 -0400

    Updates

---
+++ precache-manifest.58e16cf4958afbbbfeb3397c20a6301a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3dcf0cc1789c52e21f915362efc95f4",
+    "url": "/ghost/index.html"
+  },
+  {
+    "revision": "e20d01a4d3f953f18782",
+    "url": "/ghost/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "391976edb64406063548",
+    "url": "/ghost/static/css/main.d04181cf.chunk.css"
+  },
+  {
+    "revision": "e20d01a4d3f953f18782",
+    "url": "/ghost/static/js/2.fed41ea4.chunk.js"
+  },
+  {
+    "revision": "391976edb64406063548",
+    "url": "/ghost/static/js/main.e865e123.chunk.js"
+  },
+  {
+    "revision": "828f08d93604e403865d",
+    "url": "/ghost/static/js/runtime~main.09b4448c.js"
+  },
+  {
+    "revision": "e124389beb86619cbb7d21a5c6ddfd19",
+    "url": "/ghost/static/media/ghostLogo.e124389b.svg"
+  }
+]);
