e37d580f9187f15aaf7f0e3ce62fd55d47efb6a8
Sammy Jackson <sam.jackson@live.ca>
2019-10-06 22:55:13 -0400

    changed title and added favicon

---
+++ build/precache-manifest.94eb6fd3593498a78a8e8e2e24e420ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52a521e3185bc09eb51120f602eee588",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e20d01a4d3f953f18782",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "f945d4dbf575061f40fe",
+    "url": "/static/css/main.d04181cf.chunk.css"
+  },
+  {
+    "revision": "e20d01a4d3f953f18782",
+    "url": "/static/js/2.fed41ea4.chunk.js"
+  },
+  {
+    "revision": "f945d4dbf575061f40fe",
+    "url": "/static/js/main.e38e8d8a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e124389beb86619cbb7d21a5c6ddfd19",
+    "url": "/static/media/ghostLogo.e124389b.svg"
+  }
+]);
